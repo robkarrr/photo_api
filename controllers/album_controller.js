b48be7aec98465381db7545533d98f47d706bfa1
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-03-10 17:52:22 +0100

    user can now get a specific album

--- controllers/album_controller.js
+++ controllers/album_controller.js
@@ -25,6 +25,33 @@ const debug = require('debug')('book:album_controller');
 
  }
 
+ const loadOneAlbum = async (req, res) => {
+        
+    const album = await models.Album.fetchById(req.params.albumId, {withRelated: ['photos'], require: false});
+
+    if (!album) {
+        return res.status(404).send({
+            status: 'fail',
+            data: 'There is no such album'
+        });
+    }
+
+
+    if (album.attributes.user_id !== req.user.id) {
+        return res.status(403).send({
+            status: 'fail',
+            data: 'That is not your album!'
+        });
+    }
+
+    res.status(200).send({
+        status: 'success',
+        data: album
+    })
+
+}
+
  module.exports = {
      loadAlbums,
+     loadOneAlbum,
  }
--- routes/albums.js
+++ routes/albums.js
@@ -6,7 +6,9 @@ const albumValidationRules = require('../validation/album');
 
 // Get all albums
 router.get('/', albumController.loadAlbums);
+
 // Get a specific album
+router.get('/:albumId', albumController.loadOneAlbum);
 
 
 module.exports = router;
